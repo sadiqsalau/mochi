50e9e08bf6d7ac64c93947dc435f11f3e3b417ab
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-25 21:26:17 +0100

    Overrides colors: green

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,19 @@ module.exports = {
       fontFamily: {
         sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        green: {
+          100: "#eefae9",
+          200: "#dcf4d3",
+          300: "#cbefbc",
+          400: "#b9e9a6",
+          500: "#a8e490",
+          600: "#86b673",
+          700: "#658956",
+          800: "#435b3a",
+          900: "#222e1d",
+        },
+      },
     },
   },
   plugins: [],
