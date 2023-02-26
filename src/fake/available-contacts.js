bd7ad8e4db5fbf6fe2d72f576bfab2661b41a30c
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-26 15:21:11 +0100

    Added fake data

---
+++ src/fake/available-contacts.js
@@ -0,0 +1,29 @@
+import { userImages } from "./user-images";
+
+export const availableContacts = [
+  {
+    name: "Robert Downey Jr.",
+    photo: userImages[4],
+    status: "online",
+  },
+  {
+    name: "Mark Zuckerberg",
+    photo: userImages[8],
+    status: "online",
+  },
+  {
+    name: "Scarlett Johansson",
+    photo: userImages[12],
+    status: "online",
+  },
+  {
+    name: "Anna Paquain",
+    photo: userImages[14],
+    status: "online",
+  },
+  {
+    name: "Daniel Day Lewis",
+    photo: userImages[0],
+    status: "online",
+  },
+];
---
+++ src/fake/conversations.js
@@ -0,0 +1,41 @@
+import { userImages } from "./user-images";
+
+export const conversations = [
+  {
+    title: "Chris Hemsworth",
+    photo: userImages[13],
+    status: "busy",
+    lastMsg: "Hey . . we still doing the movie?",
+    unreadCount: 1,
+  },
+  {
+    title: "Dick Costolo",
+    photo: userImages[2],
+    status: "online",
+    lastMsg: "I checked the flights",
+    unreadCount: 0,
+  },
+
+  {
+    title: "Jeffrey Katzenberg",
+    photo: userImages[1],
+    status: "online",
+    lastMsg: "gtg let me know",
+    unreadCount: 0,
+  },
+
+  {
+    title: "Chris Evans",
+    photo: userImages[9],
+    status: "offline",
+    lastMsg: "See if you can get it done",
+    unreadCount: 0,
+  },
+  {
+    title: "Larry Page",
+    photo: userImages[7],
+    status: "offline",
+    lastMsg: "I could not wait but hope it will g.",
+    unreadCount: 0,
+  },
+];
