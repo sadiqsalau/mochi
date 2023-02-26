cb743c0a96614547b9a947b706eb753dbe543561
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-26 08:55:31 +0100

    Configured ViteEjsPlugin with loaded env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
     },
-  },
-  plugins: [react()],
+    plugins: [ViteEjsPlugin(env), react()],
+  };
 });
