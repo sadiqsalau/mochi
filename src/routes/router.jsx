716993d3f858972b48c7e2b69662c08381099278
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-26 09:05:55 +0100

    Added suspense fallback for route loaders

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,25 @@
+import { Outlet } from "react-router-dom";
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import { Suspense } from "react";
 
 import { Page } from "./page";
+import { SuspenseFallback } from "./suspense-fallback";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
+    <Route
+      element={
+        <Suspense fallback={<SuspenseFallback />}>
+          <Outlet />
+        </Suspense>
+      }
+    >
       <Route index element={<Page page="Welcome" />} />
-    </>
+    </Route>
   ),
   {
     basename: import.meta.env.BASE_URL,
---
+++ src/routes/suspense-fallback.jsx
@@ -0,0 +1,7 @@
+import { Spinner } from "@/component/spinner/spinner";
+
+export const SuspenseFallback = () => (
+  <div className="min-h-screen flex justify-center items-center">
+    <Spinner />
+  </div>
+);
