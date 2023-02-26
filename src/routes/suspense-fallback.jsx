689fd88ed773b66a7c87664747153d06feedb5e2
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-26 13:43:03 +0100

    Updated imports path

--- src/routes/suspense-fallback.jsx
+++ src/routes/suspense-fallback.jsx
@@ -1,4 +1,4 @@
-import { Spinner } from "@/component/spinner/spinner";
+import { Spinner } from "@/components/spinner/spinner";
 
 export const SuspenseFallback = () => (
   <div className="min-h-screen flex justify-center items-center">
