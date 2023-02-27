c65cc69bb772399313d9b28ab586320234fb602e
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-27 02:44:42 +0100

    Added buttons in header

--- src/pages/chats/side-area.jsx
+++ src/pages/chats/side-area.jsx
@@ -1,7 +1,12 @@
+import { ComposeIcon } from "@/icons/compose-icon";
 import { Contact } from "@/components/contact/contact";
 import { Conversation } from "@/components/conversation/conversation";
-import { Header } from "@/partials/header/header";
+import { FaChevronLeft } from "react-icons/fa";
+import { Header } from "@/components/header/header";
+import { HeaderButton } from "@/components/header/header-button";
+import { HeaderTitle } from "@/components/header/header-title";
 import { Navbar } from "@/partials/navbar/navbar";
+import { NotificationIcon } from "@/icons/notification-icon";
 import { SectionHeading } from "@/components/section-heading/section-heading";
 import { availableContacts } from "@/fake/available-contacts";
 import { conversations } from "@/fake/conversations";
@@ -15,7 +20,24 @@ export const SideArea = () => {
     <div className="h-screen overflow-auto flex flex-col md:border-r md:border-stone-200">
       {/* Header */}
       <div className="shrink-0">
-        <Header title="Your group chats" />
+        <Header
+          left={
+            <HeaderButton>
+              <FaChevronLeft />
+            </HeaderButton>
+          }
+          center={<HeaderTitle>Your group chats</HeaderTitle>}
+          right={
+            <>
+              <HeaderButton>
+                <NotificationIcon className="w-4" />
+              </HeaderButton>
+              <HeaderButton>
+                <ComposeIcon className="w-4" />
+              </HeaderButton>
+            </>
+          }
+        />
       </div>
 
       {/* Content */}
