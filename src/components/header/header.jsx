faf692d26fa8fd08977f4ccadc508567908b643c
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-27 02:06:30 +0100

    Added 3 sides for content

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,10 +3,13 @@ import clsx from "clsx";
 export const Header = (props) => (
   <div
     className={clsx(
-      "bg-white shadow-[0px_2px_2px_0px] shadow-stone-100 sticky top-0 p-2",
-      "flex justify-center items-center"
+      "relative z-10 bg-white",
+      "shadow-[0px_2px_2px_0px] shadow-stone-100",
+      "grid grid-cols-[1fr_2fr_1fr] items-center"
     )}
   >
-    <div className="text-center font-bold">{props.title}</div>
+    <div className="flex justify-start">{props.left}</div>
+    <div className="flex justify-center">{props.center}</div>
+    <div className="flex justify-end">{props.right}</div>
   </div>
 );
