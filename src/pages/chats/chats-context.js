56440d7c95f9b1c5117b1905820027ef90778465
Sadiq Salau <sadiqsalau888@gmail.com>
2023-02-28 11:51:31 +0100

    Deleted chats context

--- src/pages/chats/chats-context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { createProvider } from "@/utils/create-provider";
-
-export const { hook: useChats, provider: ChatsProvider } = createProvider(
-  createContext({})
-);
