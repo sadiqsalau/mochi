a9cfc718d149f4bf54657ab8f45bb442e694440a
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-03 16:59:42 +0100

    Set fixed height

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,9 +3,9 @@ import clsx from "clsx";
 export const Header = (props) => (
   <div
     className={clsx(
-      "relative z-10 bg-white",
+      "h-12 relative z-10 bg-white",
       "shadow-[0px_2px_2px_0px] shadow-stone-100",
-      "grid grid-cols-[1fr_2fr_1fr] items-center"
+      "grid grid-cols-[1fr_2fr_1fr]"
     )}
   >
     <div className="flex justify-start">{props.left}</div>
