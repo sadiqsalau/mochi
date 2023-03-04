07d493467f00a1243203645046daa08758dedb15
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-04 10:08:03 +0100

    Added "as" prop for dynamic component

--- src/components/header/header-button.jsx
+++ src/components/header/header-button.jsx
@@ -1,16 +1,18 @@
 import clsx from "clsx";
 import { forwardRef } from "react";
 
-export const HeaderButton = forwardRef((props, ref) => (
-  <button
-    {...props}
-    ref={ref}
-    className={clsx(
-      props.className,
-      "aspect-square inline-flex justify-center items-center",
-      "text-green-500"
-    )}
-  >
-    {props.children}
-  </button>
-));
+export const HeaderButton = forwardRef(
+  ({ as: Component = "button", ...props }, ref) => (
+    <Component
+      {...props}
+      ref={ref}
+      className={clsx(
+        props.className,
+        "aspect-square inline-flex justify-center items-center",
+        "text-green-500"
+      )}
+    >
+      {props.children}
+    </Component>
+  )
+);
