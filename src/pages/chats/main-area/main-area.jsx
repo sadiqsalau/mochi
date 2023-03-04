57384c6277c35a7ab7a6cebb9a839d8f4eef29eb
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-04 11:31:26 +0100

    Added chat menu to layout

--- src/pages/chats/main-area/main-area.jsx
+++ src/pages/chats/main-area/main-area.jsx
@@ -3,6 +3,7 @@ import { Message } from "@/components/message/message";
 import { chatMessages } from "@/fake/chat-messages";
 import { useLocation } from "react-router-dom";
 
+import { ChatMenu } from "./chat-menu";
 import { MainAreaHeader } from "./main-area-header";
 
 export const MainArea = () => {
@@ -22,11 +23,12 @@ export const MainArea = () => {
             "w-[200%] xl:w-full h-full",
             "grid grid-cols-2 grid-rows-[100%] xl:grid-cols-[1fr_400px]",
             "duration-500 xl:transform-none",
+            "xl:divide-x",
             location.state?.showMenu && "-translate-x-1/2"
           )}
         >
           {/* Messages Box */}
-          <div className="h-full flex flex-col">
+          <div className="flex flex-col">
             {/* Messages */}
             <div
               className={clsx(
@@ -44,6 +46,9 @@ export const MainArea = () => {
               <div className="bg-[#FAFFF2] p-2 flex"></div>
             </div>
           </div>
+
+          {/* Chat Menu */}
+          <ChatMenu />
         </div>
       </div>
     </div>
