8c31a66713392fc932bff3b82df109ab1ee2c388
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-04 20:06:14 +0100

    Set font weight to bold

--- src/components/section-heading/section-heading.jsx
+++ src/components/section-heading/section-heading.jsx
@@ -1,5 +1,5 @@
 export const SectionHeading = (props) => (
-  <div className="p-2 bg-[#FAFFF2] shadow-[0px_2px_2px_0px] shadow-stone-100 sticky top-0">
+  <div className="p-2 font-bold bg-[#FAFFF2] shadow-[0px_2px_2px_0px] shadow-stone-100 sticky top-0">
     {props.children}
   </div>
 );
