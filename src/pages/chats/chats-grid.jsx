686d29e312a08dc5bf55ab29af27173207bc2355
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-05 12:19:29 +0100

    Increased side area width on tablet

--- src/pages/chats/chats-grid.jsx
+++ src/pages/chats/chats-grid.jsx
@@ -10,7 +10,7 @@ export const ChatsGrid = (props) => {
         className={clsx(
           "w-[200%] md:w-full",
           "grid grid-cols-2",
-          "md:grid-cols-[320px_1fr] lg:grid-cols-[400px_1fr]",
+          "md:grid-cols-[350px_1fr] lg:grid-cols-[400px_1fr]",
           "duration-500 transition-transform md:transform-none",
           location.state?.showMainArea && "-translate-x-1/2"
         )}
