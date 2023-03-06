7ec5c08a74a6560445682af5adc563c1aa92cb38
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-06 22:03:05 +0100

    Replaced imports with glob

--- src/pages/welcome/onboarding.js
+++ src/pages/welcome/onboarding.js
@@ -1,21 +1,24 @@
-import OnboardingImage1 from "@/assets/images/onboarding/01.png";
-import OnboardingImage2 from "@/assets/images/onboarding/02.png";
-import OnboardingImage3 from "@/assets/images/onboarding/03.png";
+export const onboardingImages = Object.values(
+  import.meta.glob("@/assets/images/onboarding/*.png", {
+    eager: true,
+    as: "url",
+  })
+);
 
 export const onboarding = [
   {
     title: "connect with friends!",
     text: "Sed ut perspiciatis unde omnis iste rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.",
-    image: OnboardingImage1,
+    image: onboardingImages[0],
   },
   {
     title: "get in touch instantly",
     text: "Eaque ipsa quae abed ut perspiciatis unde omnis iste rem aperiam, eaque ipsa quae ab illo inventore verita.",
-    image: OnboardingImage2,
+    image: onboardingImages[1],
   },
   {
     title: "many platforms, one chat",
     text: "Aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae. Isterem aperiam, eaque ab illo.",
-    image: OnboardingImage3,
+    image: onboardingImages[2],
   },
 ];
