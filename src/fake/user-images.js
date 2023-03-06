62ca6e8b15a41d328332155fc82b8619e0c4f059
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-06 21:55:14 +0100

    Replaced import with glob

--- src/fake/user-images.js
+++ src/fake/user-images.js
@@ -1,37 +1,6 @@
-import UserImage01 from "@/assets/images/users/01.png";
-import UserImage02 from "@/assets/images/users/02.png";
-import UserImage03 from "@/assets/images/users/03.png";
-import UserImage04 from "@/assets/images/users/04.png";
-import UserImage05 from "@/assets/images/users/05.png";
-import UserImage06 from "@/assets/images/users/06.png";
-import UserImage07 from "@/assets/images/users/07.png";
-import UserImage08 from "@/assets/images/users/08.png";
-import UserImage09 from "@/assets/images/users/09.png";
-import UserImage10 from "@/assets/images/users/10.png";
-import UserImage11 from "@/assets/images/users/11.png";
-import UserImage12 from "@/assets/images/users/12.png";
-import UserImage13 from "@/assets/images/users/13.png";
-import UserImage14 from "@/assets/images/users/14.png";
-import UserImage15 from "@/assets/images/users/15.png";
-import UserImage16 from "@/assets/images/users/16.png";
-import UserImage17 from "@/assets/images/users/17.png";
-
-export const userImages = [
-  UserImage01,
-  UserImage02,
-  UserImage03,
-  UserImage04,
-  UserImage05,
-  UserImage06,
-  UserImage07,
-  UserImage08,
-  UserImage09,
-  UserImage10,
-  UserImage11,
-  UserImage12,
-  UserImage13,
-  UserImage14,
-  UserImage15,
-  UserImage16,
-  UserImage17,
-];
+export const userImages = Object.values(
+  import.meta.glob("@/assets/images/users/*.png", {
+    eager: true,
+    as: "url",
+  })
+);
