79bd7c373cf6f431050a3b00a5a1c69f31c24395
Sadiq Salau <sadiqsalau888@gmail.com>
2023-06-07 20:31:48 +0100

    Removed suspense

--- src/layouts/auth-page/auth-page.jsx
+++ src/layouts/auth-page/auth-page.jsx
@@ -1,7 +1,5 @@
 import Logo from "@/assets/logo.svg";
 import { Link, Outlet } from "react-router-dom";
-import { Spinner } from "@/components/spinner/spinner";
-import { Suspense } from "react";
 
 import { AuthPageContainer } from "./auth-page-container";
 
@@ -12,8 +10,6 @@ export const AuthPage = () => (
       <img src={Logo} className="w-16 h-16" />
     </Link>
 
-    <Suspense fallback={<Spinner className="mx-auto" />}>
-      <Outlet />
-    </Suspense>
+    <Outlet />
   </AuthPageContainer>
 );
