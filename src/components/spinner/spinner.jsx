2b392c1e466917e5c876fcfb30846146d286c319
Sadiq Salau <sadiqsalau888@gmail.com>
2023-06-07 20:30:03 +0100

    Added Centered and Fullscreen variant

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -3,12 +3,33 @@ import { forwardRef } from "react";
 
 import Gif from "./spinner.gif";
 
+// Base spinner
 export const Spinner = forwardRef((props, ref) => (
   <img
     {...props}
     ref={ref}
     src={Gif}
-    alt={props.alt || "Loading.."}
+    alt={props.alt || "Loading..."}
     className={clsx(props.className, "w-7 h-7")}
   />
+));
+
+
+// Centered spinner
+export const CenteredSpinner = forwardRef((props, ref) => (
+    <div className="flex justify-center">
+        <Spinner {...props} ref={ref} />
+    </div>
+));
+
+// FullScreen spinner
+export const FullScreenSpinner = forwardRef((props, ref) => (
+    <div
+        className={clsx(
+            "flex justify-center items-center",
+            "h-screen supports-[height:100dvh]:h-[100dvh]"
+        )}
+    >
+        <Spinner {...props} ref={ref} />
+    </div>
 ));
